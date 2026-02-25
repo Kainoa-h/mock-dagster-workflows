63e13bbce4fd1c15dbcd1b61f30cf7eb4ee4f740
kainoa-h <kainoahowj@gmail.com>
2026-02-25 13:12:13 +0800

    add workflow 4

--- __init__.py
+++ __init__.py
@@ -2,7 +2,8 @@ from dagster import Definitions
 from .jobs.workflow_one import run_workflow_one
 from .jobs.workflow_two import run_workflow_two
 from .jobs.workflow_three import run_workflow_three
+from .jobs.workflow_four import run_workflow_four
 
 defs = Definitions(
-    jobs=[run_workflow_one, run_workflow_two, run_workflow_three]
+    jobs=[run_workflow_one, run_workflow_two, run_workflow_three, run_workflow_four]
 )
