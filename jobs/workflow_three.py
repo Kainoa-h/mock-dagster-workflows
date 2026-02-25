d9ce1cda84a35a31bb8742451325bf3e1e73ab4a
kainoa-h <kainoahowj@gmail.com>
2026-02-25 13:05:23 +0800

    update workflow 3

--- jobs/workflow_three.py
+++ jobs/workflow_three.py
@@ -2,12 +2,11 @@ from dagster import op, job
 
 
 @op
-def workflow_three():
-    print("Hello from workflow_three")
+def workflow_three(context):
+    context.log.info("Hello from workflow_three")
     return "workflow_three done"
 
 
 @job
 def run_workflow_three():
     workflow_three()
-
