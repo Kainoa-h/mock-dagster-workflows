5c117048a8fa86882929285d744eda5afc46e667
kainoa-h <kainoahowj@gmail.com>
2026-02-25 14:29:36 +0800

    add demo workflow

--- __init__.py
+++ __init__.py
@@ -3,7 +3,14 @@ from .jobs.workflow_one import run_workflow_one
 from .jobs.workflow_two import run_workflow_two
 from .jobs.workflow_three import run_workflow_three
 from .jobs.workflow_four import run_workflow_four
+from .jobs.workflow_demo import run_workflow_demo
 
 defs = Definitions(
-    jobs=[run_workflow_one, run_workflow_two, run_workflow_three, run_workflow_four]
+    jobs=[
+        run_workflow_one,
+        run_workflow_two,
+        run_workflow_three,
+        run_workflow_four,
+        run_workflow_demo,
+    ]
 )
